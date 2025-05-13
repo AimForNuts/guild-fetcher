013d0c0186bb88221607a4c5d573350ac8ba3648
AimForNuts <josepsloliveira@gmail.com>
2025-05-13 01:22:18 +0100

    Remove idle-game from repo and add to .gitignore

--- idle-game/migrations/000003_add_default_character.sql
+++
@@ -1,4 +0,0 @@
--- Add default character
-INSERT INTO characters (name, woodcutting_level, woodcutting_xp)
-VALUES ('User', 1, 0)
-ON CONFLICT (name) DO NOTHING; 
--- idle-game/migrations/000004_add_avatar_url.sql
+++
@@ -1,2 +0,0 @@
--- Add avatar_url column to characters table
-ALTER TABLE characters ADD COLUMN avatar_url VARCHAR(255) DEFAULT 'https://api.dicebear.com/7.x/avataaars/svg?seed=default'; 
